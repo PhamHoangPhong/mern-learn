445c25e0a832c2efdc8a9eeeec21ec6612b7fd1c
NagoriYuki <phongkzs1999@gmail.com>
2022-02-22 10:21:59 +0700

    prepare deploy

--- client/src/contexts/constant.js
+++ client/src/contexts/constant.js
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : ''
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://young-ridge-91377.herokuapp.com/api'
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mearn'
